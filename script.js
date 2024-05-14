510a68d1d5f3db943fc2ff49ad958e0f159bc839
Ashley Wright <ashleywright@iMac.local>
2024-05-14 15:18:28 -0400

    added collect employees function to script

--- script.js
+++ script.js
@@ -3,11 +3,32 @@ const addEmployeesBtn = document.querySelector('#add-employees-btn');
 
 // Collect employee data
 const collectEmployees = function() {
+  const employees = [];
+
+  let addEmployee = true;
+  while(addEmployee) {
+    const firstName = prompt("Enter employee's first name:");
+    const lastName = prompt("Enter employee's last name:");
+    let salary = prompt("Enter employee's salary");
+    salary = isNaN(Number(salary)) ? 0: Number(salary);
+
+    employees.push({
+      firstName: firstName,
+      lastName: lastName,
+      salary: salary
+    });
+
+    const continueAdding = confirm("Do you want to add another employee?");
+    addEmployee = continueAdding;
+  }
+
+  return employees;
   // TODO: Get user input to create and return an array of employee objects
 }
 
 // Display the average salary
 const displayAverageSalary = function(employeesArray) {
+  
   // TODO: Calculate and display the average salary
 }
 
